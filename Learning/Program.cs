5fb86945d3a8acb93b6c6cbf6cbf8ff41a5c3188
julianxrenner <julesman18@gmail.com>
2026-04-03 06:10:53 -0400

    simple calculator getting familiar with syntax

---
+++ Learning/Program.cs
@@ -0,0 +1,42 @@
+﻿while (true)
+{
+  Console.WriteLine("Enter a number:");
+  string? firstVal = Console.ReadLine();
+  int val1 = Int32.Parse(firstVal);
+  Console.WriteLine("Enter another number:");
+  string? secVal = Console.ReadLine();
+  int val2 = Int32.Parse(secVal);
+  Console.WriteLine("Enter an operator");
+  string? op = Console.ReadLine();
+
+  int total = 0;
+
+  switch (op)
+  {
+    case "+":
+      total = val1 + val2;
+      break;
+    case "-":
+      total = val1 - val2;
+      break;
+    case "/":
+      total = val1 / val2;
+      break;
+    case "*":
+      total = val1 * val2;
+      break;
+    default:
+      break;
+  }
+
+  Console.WriteLine($"The Result is: {total}");
+  Console.WriteLine("Would you like to calculate again? say \"no\" to stop.");
+  string? ans = Console.ReadLine();
+  if (ans == "no"){
+    Console.WriteLine("Oh man... Bye Bye.");
+    break;
+  }else{
+    continue;
+  }
+}
+
