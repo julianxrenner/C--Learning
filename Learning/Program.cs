b28f4b107cf615a944f88008bde7ee0a6b90e271
julianxrenner <julesman18@gmail.com>
2026-04-06 05:09:39 -0400

    learning C# classes

--- Learning/Program.cs
+++ Learning/Program.cs
@@ -1,42 +1,16 @@
-﻿while (true)
+﻿Person person1 = new Person("Julian");
+Person person2 = new Person("Renenr");
+
+Console.WriteLine(person1.Name);
+
+class Person
 {
-  Console.WriteLine("Enter a number:");
-  string? firstVal = Console.ReadLine();
-  int val1 = Int32.Parse(firstVal);
-  Console.WriteLine("Enter another number:");
-  string? secVal = Console.ReadLine();
-  int val2 = Int32.Parse(secVal);
-  Console.WriteLine("Enter an operator");
-  string? op = Console.ReadLine();
 
-  int total = 0;
-
-  switch (op)
+  public Person(string name)
   {
-    case "+":
-      total = val1 + val2;
-      break;
-    case "-":
-      total = val1 - val2;
-      break;
-    case "/":
-      total = val1 / val2;
-      break;
-    case "*":
-      total = val1 * val2;
-      break;
-    default:
-      break;
+    Name = name;
   }
 
-  Console.WriteLine($"The Result is: {total}");
-  Console.WriteLine("Would you like to calculate again? say \"no\" to stop.");
-  string? ans = Console.ReadLine();
-  if (ans == "no"){
-    Console.WriteLine("Oh man... Bye Bye.");
-    break;
-  }else{
-    continue;
-  }
+  public string Name { get; set; }
+
 }
-
