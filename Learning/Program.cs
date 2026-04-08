73adb8d3d6b5426a4da72f8bb05a468fbfac7aa6
julianxrenner <julesman18@gmail.com>
2026-04-08 10:54:24 -0400

    adding more features to simple accounting app

--- Learning/Program.cs
+++ Learning/Program.cs
@@ -1,24 +1,56 @@
 ﻿
 
-Account myAccount = new Account(123, 21);
-Console.WriteLine(myAccount.Balance);
+Account regularAccount = new Account(123, 210, false);
+Account premiumAccount = new Account(456, 210, true);
 
-myAccount.DepositMoney(50);
-Console.WriteLine(myAccount.Balance);
+Console.WriteLine($"Regular Checking Account: {regularAccount.Balance}");
+Console.WriteLine($"Premium Checking Account: {premiumAccount.Balance}");
+
+regularAccount.AddInterest();
+premiumAccount.AddInterest();
+
+Console.WriteLine($"Regular Checking Account After Interest: {regularAccount.Balance}");
+Console.WriteLine($"Premium Checking Account After Interest: {premiumAccount.Balance}");
+
+regularAccount.TransferMoney(200m, premiumAccount);
+
+Console.WriteLine($"Regular Checking Account After Transfer: {regularAccount.Balance}");
+Console.WriteLine($"Premium Checking Account After Transfer: {premiumAccount.Balance}");
 
 class Account
 {
   public int IdentificationNumber { get; set; }
   public decimal Balance { get; set; }
-  public Account(int identificationNumber, decimal balance)
+
+  public bool IsPremium { get; set; }
+
+  private decimal InterestRate { get; set; }
+  public Account(int identificationNumber, decimal balance, bool isPremium)
   {
     IdentificationNumber = identificationNumber;
     Balance = balance;
+    IsPremium = isPremium;
+    InterestRate = isPremium ? 0.03m : 0.02m;
   }
 
   public void DepositMoney(decimal depositAmount)
   {
     Balance += depositAmount;
+  }
+  public void AddInterest()
+  {
+    Balance += Balance * InterestRate;
+  }
+
+  public void TransferMoney(decimal transferAmount, Account receivingAccount)
+  {
+    if (Balance < transferAmount || receivingAccount == null)
+    {
+      Console.WriteLine("Invalid arguments");
+      return;
+    }
+    Balance -= transferAmount;
+    receivingAccount.DepositMoney(transferAmount);
   }
 
 }
--- Learning/obj/Debug/net10.0/Learning.AssemblyInfo.cs
+++ Learning/obj/Debug/net10.0/Learning.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Learning")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8ac155ca03b24266556138b2881fc1db347003d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5c01054a5a497e54c12b3bbdf51ad630dc52829")]
 [assembly: System.Reflection.AssemblyProductAttribute("Learning")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Learning")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
